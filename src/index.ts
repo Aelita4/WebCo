cf83b7f2fbd2b8d41c388e7347147f2799b38d0e
Aelita4 <kontakt@mikorosa.pl>
2023-06-17 16:28:21 +0200

    Add login/register functionality

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ const db = new Database();
 
 app.set('views', __dirname + "../views");
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "../views/public"));
 app.use(session({
   name: 'webcolony',
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import db from "../index.js";
+import bcrypt from 'bcrypt';
 
 const router = Router();
 const path = "/login";
@@ -9,6 +10,66 @@ router.get(path, async (req: Request, res: Response) => {
     // if(req.session.user) user = await db.select(`users:${req.session.user}`);
     res.render('pages/login.ejs');
 });
+
+router.post(`${path}/log`, async (req: Request, res: Response) => {
+    const username = req.body['login'];
+    const password = req.body['password'];
+
+    const ifFound = await db.select(`users:${username}`);
+    if(typeof ifFound === 'undefined') {
+        res.redirect('/login?form=login&error=invalidCredentials');
+        return;
+    }
+
+    const hash = ifFound.password;
+    const isPasswordCorrect = await bcrypt.compare(password, hash);
+    if(!isPasswordCorrect) {
+        res.redirect('/login?form=login&error=invalidCredentials');
+        return;
+    }
+
+    req.session['user'] = username;
+    res.redirect('/game');
+});
+
+router.post(`${path}/reg`, async (req: Request, res: Response) => {
+    const username = req.body['login'];
+    const email = req.body['email'];
+    const password = req.body['password'];
+    const password2 = req.body['password2'];
+
+    if(password !== password2) {
+        res.redirect('/login?form=register&error=passwordsNotMatch');
+        return;
+    }
+
+    const ifFound = await db.selectWhere('users', `username = '${username}' OR email = '${email}'`);
+    if(typeof ifFound !== 'undefined') {
+        res.redirect('/login?form=register&error=userExists');
+        return;
+    }
+
+    const hash = await encryptPassword(password);
+
+    await db.create(`users:${username}`, {
+        username,
+        email,
+        password: hash
+    });
+
+    req.session['user'] = username;
+    res.redirect('/game');
+});
+
+async function encryptPassword(password: string) {
+    const hash = await bcrypt.hash(password, 10);
+    return hash;
+}
+
+async function validatePassword(password: string, hash: string) {
+    const result = await bcrypt.compare(password, hash);
+    return result;
+}
 
 export default {
     path,
