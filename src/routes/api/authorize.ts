ee0470942ef3e201e3167b1a98bda21513eb3280
Aelita4 <kontakt@mikorosa.pl>
2023-06-28 00:10:45 +0200

    Add API authorization endpoint

---
+++ src/routes/api/authorize.ts
@@ -0,0 +1,41 @@
+import { Router, Request, Response } from "express";
+import db from "../../index.js";
+import User from "../../types/User.js";
+import bcrypt from 'bcrypt';
+
+const router = Router();
+const path = "/api/authorize";
+
+router.post(`${path}`, async (req: Request, res: Response) => {
+    const username = req.body['login'];
+    const password = req.body['password'];
+
+    const ifFound: User = await db.selectWhere(`users`, `username = '${username}' OR email = '${username}'`);
+    if(typeof ifFound === 'undefined') {
+        res.status(401).json({ code: 401, message: "Unauthorized" });
+        return;
+    }
+
+    const hash = ifFound.password;
+    const isPasswordCorrect = await validatePassword(password, hash || '');
+    if(!isPasswordCorrect) {
+        res.status(401).json({ code: 401, message: "Unauthorized" });
+        return;
+    }
+
+    await db.merge(ifFound.id, {
+        lastSeen: new Date().getTime()
+    });
+
+    res.status(200).json({ code: 200, message: "OK", accessToken: ifFound.tmp });
+});
+
+async function validatePassword(password: string, hash: string) {
+    const result = await bcrypt.compare(password, hash);
+    return result;
+}
+
+export default {
+    path,
+    router
+};
